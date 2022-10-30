64b570d9ae6785fbab38bb70b837aea9970baf7a
lekolawole <lekolawole@gmail.com>
2022-10-30 14:07:09 -0400

    Made SectionLayout component more dynamic

--- website/pages/index.js
+++ website/pages/index.js
@@ -2,14 +2,47 @@ import Head from 'next/head';
 import Image from 'next/image';
 import SectionLayout from '../components/section-layout/section-layout';
 import styles from '../styles/Home.module.css';
+import siteContent from '../public/PublicVariables';
 
 export default function Home() {
+  const Titles = siteContent.sectionTitles;
+  const Subtitles = siteContent.sectionSubtitles;
+  const SectionImg = siteContent.sectionImages;
   return (
     <>
-      <div className={styles.body}>
-        <SectionLayout></SectionLayout>
-        <SectionLayout></SectionLayout>
-        <SectionLayout></SectionLayout>
+      <div className={styles.main}>
+        <SectionLayout 
+          title={Titles.title1} 
+          subtitle={Subtitles.subtitle1}
+          img={SectionImg.astronaut}
+        ></SectionLayout>
+        <SectionLayout 
+          title={Titles.title2}
+          subtitle={Subtitles.subtitle2}></SectionLayout>
+        <SectionLayout 
+          title={Titles.title3}
+          subtitle={Subtitles.subtitle3}></SectionLayout>
+        <SectionLayout 
+          title={Titles.title4}
+          subtitle={Subtitles.subtitle4}></SectionLayout>
+        <SectionLayout 
+          title={Titles.title5}
+          subtitle={Subtitles.subtitle5}></SectionLayout>
+        <SectionLayout 
+          title={Titles.title6}
+          subtitle={Subtitles.subtitle6}></SectionLayout>
+        <SectionLayout 
+          title={Titles.title7}
+          subtitle={Subtitles.subtitle7}></SectionLayout>
+        <SectionLayout 
+          title={Titles.title8}
+          subtitle={Subtitles.subtitle8}></SectionLayout>
+        <SectionLayout 
+          title={Titles.title9}
+          subtitle={Subtitles.subtitle9}></SectionLayout>
+        <SectionLayout 
+          title={Titles.title10}
+          subtitle={Subtitles.subtitle10}></SectionLayout>
       </div>
       
     </>

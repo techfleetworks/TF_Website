88ac7c837a7307516d0606280cf32e520ba7ad68
lekolawole <lekolawole@gmail.com>
2022-11-01 19:12:06 -0400

    adding sections layout

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/navbar/navbar"
 import Banner from "../components/sections/homepage/banner/banner"
+import bothsections from "../components/sections/homepage/chosepath-helpingppl/bothsections"
 
 export default function Home() {
   return (
     <>
       <Navbar/>
       <Banner/>
+      <bothsections/>
     </>
    
   )
